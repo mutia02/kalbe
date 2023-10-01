43055dba396daac8d21ba7d55fc2efc5332a6db8
Mutiara <146419975+mutia02@users.noreply.github.com>
2023-10-01 14:41:31 +0700

    Update query.sql

--- sql/query.sql
+++ sql/query.sql
@@ -1,15 +1,19 @@
 SELECT driver_id, COUNT(*) AS total_shipments
-FROM shipment_product
-WHERE EXTRACT(MONTH FROM sending_time) = 5 AND EXTRACT(YEAR FROM sending_time) = 2023
+FROM app."shipment_product" AS sp
+JOIN app."shipment" AS s
+ON sp.shipment_id = s.id
+WHERE EXTRACT(MONTH FROM s.sending_time) = 5 AND EXTRACT(YEAR FROM s.sending_time) = 2023
 GROUP BY driver_id
 ORDER BY total_shipments DESC
 LIMIT 2;
 
 
-SELECT product_id, COUNT(*) AS total_shipments
-FROM shipment_product
-WHERE EXTRACT(MONTH FROM sending_time) = 5 AND EXTRACT(YEAR FROM sending_time) = 2023
-GROUP BY product_id
+SELECT sp.product_id, COUNT(*) AS total_shipments
+FROM app."shipment_product" AS sp
+JOIN app."shipment" AS s
+ON sp.shipment_id = s.id
+WHERE EXTRACT(MONTH FROM s."sending_time") = 5 AND EXTRACT(YEAR FROM s."sending_time") = 2023
+GROUP BY sp.product_id
 ORDER BY total_shipments DESC
 LIMIT 10;
 
